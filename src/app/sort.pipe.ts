611a396467a05c1b57a0df0f8ca579def9c00eb6
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-10-04 09:42:47 +0200

    feat: Pure & Impure Pipes

--- src/app/sort.pipe.ts
+++ src/app/sort.pipe.ts
@@ -2,10 +2,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'sort',
-  standalone: true
+  standalone: true,
+  pure: false
 })
 export class SortPipe implements PipeTransform {
-
   transform(
     value: string[] | number[],
     direction: 'asc' | 'desc' = 'asc',
