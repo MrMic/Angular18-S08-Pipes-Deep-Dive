946539dadaf683fa4f38a7f0f19a3bbf712ac63d
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-10-04 09:30:41 +0200

    feat: Build a Pipe that sort s items

---
+++ src/app/sort.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sort',
+  standalone: true
+})
+export class SortPipe implements PipeTransform {
+
+  transform(
+    value: string[] | number[],
+    direction: 'asc' | 'desc' = 'asc',
+  ) {
+    const sorted = [...value].sort((a, b) => {
+      if (a > b) {
+        return direction === 'asc' ? 1 : -1;
+      }
+      if (a < b) {
+        return direction === 'asc' ? -1 : 1;
+      }
+      return 0;
+    });
+    return sorted;
+  }
+
+}
